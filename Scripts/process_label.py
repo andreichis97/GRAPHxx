65b4c7283fe74bac5b94609405e8b4b405bf9563
andreichis97 <46498920+andreichis97@users.noreply.github.com>
2023-09-04 20:48:52 +0300

    Added files

---
+++ Scripts/process_label.py
@@ -0,0 +1,48 @@
+import random
+
+readFile = open("D:\\Andrei\\ModellingTools\\Graph_Modelling_Tool\\Scripts\\label_value.txt", "r")
+writeFile = open("D:\\Andrei\\ModellingTools\\Graph_Modelling_Tool\\Scripts\\processed_uri.txt", "w")
+rawLabel = readFile.read()
+
+spaceCounter = 0
+spacePositions = []
+letterCounter = 0
+processedUri = ""
+firstPartUri = ""
+secondPartUri = ""
+#print(rawLabel)
+#writeFile.write(rawLabel)
+
+labelLetters = list(rawLabel.strip(" "))
+print(labelLetters)
+
+for i in labelLetters:
+    letterCounter = letterCounter + 1
+    if i == " ":
+        spaceCounter = spaceCounter + 1
+        spacePositions.append(letterCounter)
+        if(spaceCounter == 1):
+            slicer = slice(letterCounter-1)
+            firstPartUri = labelLetters[slicer]
+    if(spaceCounter == 0):
+        slicer = slice(letterCounter)
+        firstPartUri = labelLetters[slicer]
+
+#print(firstPartUri)
+#print(spacePositions)
+processedUri = ''.join(firstPartUri)
+#print(processedUri)
+
+for i in spacePositions:
+    secondPartUri = secondPartUri + labelLetters[i]
+#print(secondPartUri)
+
+randomIdentifier = (random.randint(0, 999999))
+processedUri = processedUri + secondPartUri + "_" + str(randomIdentifier)
+print(processedUri)
+
+writeFile.write(processedUri)
+
+
+readFile.close()
+writeFile.close()
---
+++ Scripts/upload_graphdb.py
@@ -0,0 +1,38 @@
+import requests
+
+readFile = open("D:\\Andrei\\ModellingTools\\Graph_Modelling_Tool\\Scripts\\repository_location.txt", "r")
+lines = readFile.readlines()
+# Set the repository ID and graph URI
+repositoryID = lines[0].strip()
+graphURI = lines[2].strip()
+readFile.close()
+
+
+# Define the API endpoint
+url = 'http://localhost:7200/repositories/{repositoryID}/rdf-graphs/service?graph={graphURI}'
+
+# Set the path to the Turtle file
+turtleFilePath = 'D:/Andrei/ModellingTools/Graph_Modelling_Tool/Scripts/content_graph.ttl'
+
+# Open the Turtle file and read its content
+with open(turtleFilePath, 'r') as file:
+    turtleContent = file.read()
+
+# Set the headers for the request
+headers = {
+    'Content-Type': 'application/x-turtle'
+}
+
+# Make the POST request to load the Turtle file
+response = requests.post(
+    url.format(repositoryID=repositoryID, graphURI=graphURI),
+    headers=headers,
+    data=turtleContent
+)
+
+# Check the response status code
+print(response.status_code)
+if response.status_code == 200:
+    print('Turtle file loaded successfully.')
+else:
+    print('Error loading Turtle file:', response.text)
